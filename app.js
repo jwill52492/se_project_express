04b185cc9b8fa9924aecfe751fb66cc1745809fe
jwill52492 <jwill52492@gmail.com>
2025-07-17 07:49:17 -0400

    15

--- app.js
+++ app.js
@@ -17,22 +17,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
   })
   .catch(console.error);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Server will crash now');
-  }, 0);
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://wtwr.baselinux.net',
+  'https://www.wtwr.baselinux.net',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204,
+};
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
 });
 
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://wtwr.baselinux.net',
-    'https://www.wtwr.baselinux.net',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 app.use(express.json());
 
 app.use(requestLogger);
